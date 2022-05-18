36fa5fb3c12befece416fc299b83441109d2d029
Styrbjörn Andersson <anders@akr.se>
2022-05-18 20:43:10 +0200

    Adding main

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+	        h_map = {}
+	        for i in range(len(nums)):
+	            if nums[i] in h_map:
+	                return [i, h_map[nums[i]]]
+	            else:
+	                h_map[target - nums[i]] = i
+		
+
+			
